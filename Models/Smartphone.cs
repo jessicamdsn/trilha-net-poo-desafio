332b6b471fe6824c5e171b1cbf85bd04a217d281
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2023-12-29 10:28:46 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,15 +1,20 @@
+using System.Runtime.InteropServices;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+       public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
         public void Ligar()
         {
@@ -19,6 +24,35 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+         public void NumeroSmartphone()
+        {
+            Console.WriteLine($"Numero desse dispositivo é {Numero}");
+        }
+         public void TrocarNumero()
+        {
+            bool condicao = true;
+
+            while(condicao ==true){
+
+            Console.WriteLine($"Esse aparelho trocou de número? (sim ou nao)");
+            string resposta = Console.ReadLine();
+
+            if(resposta == "sim"){
+
+                Console.WriteLine("Digite o novo número:");
+                Numero = Console.ReadLine();
+                NumeroSmartphone();
+                condicao = false;
+
+            } else if ( resposta== "nao"){
+                Console.WriteLine("OK!");
+                condicao = false;
+
+            }else{
+                Console.WriteLine("Digite uma resposta válida");
+            }
+            }
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone Iphone = new Iphone( numero: "4002-8922", modelo: "modelo 1", imei: "11111111", memoria:128 );
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("WoozWorld");
+Iphone.NumeroSmartphone();
+Iphone.TrocarNumero();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "0800-770-7900", modelo: "Modelo 2", imei: "222222222", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Huji");
+nokia.NumeroSmartphone();
+nokia.TrocarNumero();
+
+Console.WriteLine("\n");
